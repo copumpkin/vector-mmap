02d97fb4506905c0aa1b97de1c81257129429cb8
Florian Hofmann <fho@f12n.de>
2016-08-02 15:03:45 +0200

    added mmapped writing and tests

--- Data/Vector/Storable/MMap.hs
+++ Data/Vector/Storable/MMap.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
 module Data.Vector.Storable.MMap (
   System.IO.MMap.Mode(..),
   unsafeMMapMVector,
-  unsafeMMapVector
+  unsafeMMapVector,
+  writeMMapVector
 ) where
 
 import System.IO.MMap
@@ -10,8 +12,13 @@ import Foreign.Storable
 
 import qualified Data.Vector.Storable as I
 import qualified Data.Vector.Storable.Mutable as M
+
+import qualified Data.Vector.Generic as G
+
+
 import Data.Int
 
+import Control.Monad
 import Control.Monad.Primitive
 
 -- | Map a file into memory as a mutable vector.
@@ -36,3 +43,18 @@ unsafeMMapVector path range =
           Nothing -> Nothing
           Just (start, length) -> Just (start, length * sizeOf (undefined :: a))
      return $ I.unsafeFromForeignPtr foreignPtr offset (size `div` sizeOf (undefined :: a))
+
+
+-- | Write a file from a vector
+-- Be careful with existing files, parts behind the mapped range will stay
+-- as they are before the write operation.
+writeMMapVector :: forall v a. (Storable a, G.Vector v a) 
+                => FilePath -- ^ Path of the file to map
+                -> v a      -- ^ Vector to write
+                -> IO ()
+writeMMapVector path src = do
+    let len = G.length src
+    target <- unsafeMMapMVector path ReadWriteEx (Just (0,len))
+    I.copy target (G.convert src)
+
+{-# INLINABLE writeMMapVector #-}
